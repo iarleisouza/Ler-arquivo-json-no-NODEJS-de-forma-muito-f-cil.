9759c05da663f02ffb6045ed7b630b8525ec5f7b
iarleisouza <65626531+iarleisouza@users.noreply.github.com>
2024-03-16 17:13:33 -0300

    Add files via upload

---
+++ leitura-json.js
@@ -0,0 +1,7 @@
+// const [{ produto, marca, cor }] = require('./dados.json')
+
+// console.log(`O produto é ${produto}, da marca ${marca} na cor ${cor}`)
+
+const [{ produto, marca, cor }] = require('./dados2.json')
+
+console.log(`O produto é ${produto}, da marca ${marca} na cor ${cor}`)
